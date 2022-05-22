4cd9566af337850df4e6835e6ab3a41ad4796682
Emiline <emissadev@gmail.com>
2022-05-22 00:23:26 -0700

    First deploy on server

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000/";
-// export const BASE_URL = "https://egbikope.org/";
+// export const BASE_URL = "http://localhost:3000/";
+export const BASE_URL = "https://www.egbikope.org/";
 
 export const COUNTER = 3;
 export const EMAIL_PATTERN = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i;
